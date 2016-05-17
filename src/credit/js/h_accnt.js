fb9a2f751132ad9c1a238d00cfe4c894b222ccb5
syusyu02 <syuduckdv@hotmail.com>
2016-05-17 22:29:40 +0900

    before refactoring

--- src/credit/js/h_accnt.js
+++ src/credit/js/h_accnt.js
@@ -4,7 +4,7 @@ var h_accnt = (function () {
     'use strict';
     var initModule = function ( $container ) {
         h_accnt.shell.initModule($container);
-        h_accnt.data.initModule();
+        h_accnt.model.initModule();
     };
 
     return { initModule: initModule };
---
+++ src/credit/js/h_accnt.model.js
@@ -0,0 +1,13 @@
+
+h_accnt.model = (function () {
+    'use strict';
+    var
+        initModule;
+
+    initModule = function () {
+        h_accnt.data.initModule();
+    };
+
+
+    return { initModule: initModule };
+}());
